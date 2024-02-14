6a521b1156a2113e5cf1bb720ff742ea4ed76431
NubOK <tibets1995@mail.ru>
2024-02-15 00:14:38 +0400

    ProductClient

--- src/v2/Clients/ProductClient.php
+++ src/v2/Clients/ProductClient.php
@@ -2,6 +2,8 @@
 
 namespace Zendrop\ClickFunnelsApiClient\v2\Clients;
 
+use Zendrop\ClickFunnelsApiClient\Http\Packs\HttpMethod;
+use Zendrop\ClickFunnelsApiClient\v2\DTO\Products\CreateProductDTO;
 use Zendrop\ClickFunnelsApiClient\v2\DTO\Products\ProductDTO;
 
 class ProductClient extends AbstractClient implements ProductClientInterface
@@ -11,7 +13,32 @@ class ProductClient extends AbstractClient implements ProductClientInterface
      */
     public function getList(): array
     {
-        $response = $this->sendRequest('/products');
+        $response = $this->sendRequest(resource: '/products', workspaceRequest: true);
         return ProductDTO::arrayFromResponse($response->json());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getById(int $id): ProductDTO
+    {
+        $response = $this->sendRequest("/products/{$id}");
+        return ProductDTO::fromResponse($response->json());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function create(CreateProductDTO $payload): ProductDTO
+    {
+        $response = $this->sendRequest(
+            resource: '/products',
+            method: HttpMethod::POST,
+            payload: [
+                'product' => $payload->toArray(),
+            ],
+            workspaceRequest: true,
+        );
+        return ProductDTO::fromResponse($response->json());
     }
 }
---
+++ tests/Unit/v2/TestData/Products/ProductTestData.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Zendrop\ClickFunnelsApiClient\Tests\Unit\v2\TestData\Products;
+
+use JsonException;
+
+final class ProductTestData
+{
+    /**
+     * @param int $id
+     * @param string $name
+     * @param bool $visibleInStore
+     * @param string $seoTitle
+     *
+     * @return array<string, mixed>
+     * @throws JsonException
+     */
+    public static function product(
+        int $id = 10130,
+        string $name = "My Workspace's First Product",
+        bool $visibleInStore = false,
+        string $seoTitle = 'Test Product',
+    ): array {
+        $jsonResponse = <<<JSON
+        {
+            "id":10130,
+            "public_id":"jvxrVN",
+            "workspace_id":1718,
+            "name":"My Workspace's First Product",
+            "current_path":"/my-workspace-s-first-product",
+            "archived":false,
+            "visible_in_store":true,
+            "visible_in_customer_center":null,
+            "image_id":null,
+            "seo_title":null,
+            "seo_description":null,
+            "seo_image_id":null,
+            "commissionable":true,
+            "image_ids":[],
+            "variant_ids":[16061],
+            "price_ids":[10631],
+            "tag_ids":[851],
+            "redirect_funnel_id":null,
+            "cancellation_funnel_url":null,
+            "created_at":"2024-01-30T15:38:03.144Z",
+            "updated_at":"2024-01-31T21:39:22.457Z"
+        }
+JSON;
+
+        $data = json_decode($jsonResponse, true, 512, JSON_THROW_ON_ERROR);
+        $data['id'] = $id;
+        $data['name'] = $name;
+        $data['visible_in_store'] = $visibleInStore;
+        $data['seo_title'] = $seoTitle;
+
+        return $data;
+    }
+}
