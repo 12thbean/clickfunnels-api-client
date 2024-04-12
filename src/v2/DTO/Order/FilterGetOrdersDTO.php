36a638816faed0dbf6a4107e379b9b1c32956c8f
yarad <saigopr@yandex.ru>
2024-04-12 10:46:42 +0000

    [ZD-6130] - fulfillment restock dto

--- src/v2/DTO/Order/FilterGetOrdersDTO.php
+++ src/v2/DTO/Order/FilterGetOrdersDTO.php
@@ -3,14 +3,18 @@
 namespace Zendrop\ClickFunnelsApiClient\v2\DTO\Order;
 
 use Zendrop\ClickFunnelsApiClient\v2\DTO\BaseDTO;
+use Zendrop\Data\Skippable;
 
 class FilterGetOrdersDTO extends BaseDTO
 {
     public function __construct(
-        /** @var string|array<int,int>|null $id */
-        public readonly string|array|null $id,
-        /** @var string|array<int,int>|null $contactId */
-        public readonly string|array|null $contactId,
+        /** @var string|int[]|Skippable $id */
+        public readonly string|array|Skippable $id = Skippable::Skipped,
+
+        /** @var string|int[]|Skippable $contactId */
+        public readonly string|array|Skippable $contactId = Skippable::Skipped,
+
+        public readonly string|Skippable $fulfillmentStatus = Skippable::Skipped,
     ) {
     }
 }
