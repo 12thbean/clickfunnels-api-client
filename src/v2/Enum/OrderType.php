403b7316e0f47a8710582dc4e3c68d8f8e4541f3
yarad <saigopr@yandex.ru>
2024-04-16 13:19:52 +0000

    [ZD-66421] - order type subscription fix

--- src/v2/Enum/OrderType.php
+++ src/v2/Enum/OrderType.php
@@ -5,5 +5,5 @@ namespace Zendrop\ClickFunnelsApiClient\v2\Enum;
 enum OrderType: string
 {
     case OneTime = 'one-time-order';
-    case Subscription = 'subscription';
+    case Subscription = 'subscription-order';
 }
